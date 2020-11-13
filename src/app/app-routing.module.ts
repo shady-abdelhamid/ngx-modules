5b4d1a8975b9cd236cdc68d514a35a5fe255f9c6
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-11-13 10:07:56 +0200

    Add top of page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: '', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
