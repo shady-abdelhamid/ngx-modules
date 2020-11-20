f4fe99adc4f45e0275afdf468f1fe1b643c1e78f
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-11-20 18:34:16 +0200

    Add credit card formatter pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -5,10 +5,15 @@ import { PipesRoutingModule } from './pipes-routing.module';
 import { PipesComponent } from './pipes.component';
 import { TruncatePipe } from './truncate.pipe';
 import { SharedModule } from '../shared/shared.module';
+import { CreditCardFormatterPipe } from './credit-card-formatter.pipe';
 
 
 @NgModule({
-  declarations: [PipesComponent, TruncatePipe],
+  declarations: [
+    PipesComponent,
+    TruncatePipe,
+    CreditCardFormatterPipe
+  ],
   imports: [
     CommonModule,
     SharedModule,
