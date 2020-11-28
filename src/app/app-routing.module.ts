65451ba48872f2391f372b722586c0c6f65b7b82
Shady Abdelhamid <shady.abdelhamid@outlook.com>
2020-11-28 14:43:14 +0200

    Add lazy loaded service module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) }, { path: 'pipes', loadChildren: () => import('./pipes/pipes.module').then(m => m.PipesModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) }, { path: 'pipes', loadChildren: () => import('./pipes/pipes.module').then(m => m.PipesModule) }, { path: 'services', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
